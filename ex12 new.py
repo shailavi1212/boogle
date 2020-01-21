e6594e7b725ceb64a51002afd8c17bb3bade5151
eldadch <59150092+eldadch@users.noreply.github.com>
2020-01-21 18:41:59 +0200

    Add files via upload

--- ex12 new.py
+++ ex12 new.py
@@ -1,5 +1,5 @@
 import sys
-import boggle_board_randomizer
+import boggle_board_randomizer as boo
 import time
 import Graphics
 
@@ -28,6 +28,14 @@ class Board(object):
                 words.append(line.strip())
             return words
 
+    def convert_to_coor(self):
+        coor_lst = [(i, j) for i in range(4) for j in range(4)]
+        coor_lst = [coor_lst[x:x + 4] for x in range(0, len(coor_lst), 4)]
+        dict_letter_coor = {}
+        for i in range(4):
+            for j in range(4):
+                dict_letter_coor[coor_lst[i][j]] = self.board[i][j]
+        return dict_letter_coor
 
 class Cube(object):
     def __init__(self, size, face, shape):
@@ -37,20 +45,23 @@ class Cube(object):
 
 
 class Player(object):
+    """
+
+    """
     def __init__(self, name):
         self.name = name
         self.score = 0
-        self.word_list =
+        self.word_list = []
 
     def number_of_words(self):
         return len(self.word_list)
 
-    def add_word(self, word, points):
-        self.word_list.add(word)
-        self.calculate_score(points)
+    def add_word(self, word):
+        self.word_list.append(word)
+        self.calculate_score(word)
 
-    def calculate_score(self, amount):
-        self.score += amount
+    def calculate_score(self, word):
+        return len(word) ** 2
 
 
 # make sure word is valid
@@ -103,3 +114,15 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+def check_if_valid(self,coor_lst):
+
+    if len(coor_lst) == len(set(coor_lst)):
+        return True
+    else:
+        return False
+def cauculate_score(self):
