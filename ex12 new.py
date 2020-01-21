09fbea5a27c7ace119366018c9edc044035d4159
eldadch <59150092+eldadch@users.noreply.github.com>
2020-01-21 20:05:02 +0200

    Add files via upload

--- ex12 new.py
+++ ex12 new.py
@@ -1,5 +1,5 @@
 import sys
-import boggle_board_randomizer as boo
+import boggle_board_randomizer
 import time
 import Graphics
 
@@ -8,121 +8,83 @@ SEC_MSG = "Great job! keep going :)"
 LOS_MSG = "There is no word like this, try again"
 QUIT_MSG = "Thanks for playing! hope to see you soon"
 BOARD_SIZE = 4
+FILEPATH = ()
 
+def main():
+    board = boggle_board_randomizer.randomize_board()
+    dict_board = convert_to_coor(board)
+    word_list = words_list(FILEPATH)
+    used_list = []
+    score = 0
+    coor_input = input() # list of coor
+    word_input = convert_to_word(user_input,dict_board)
+    seconds = time.time()
+    while seconds:
+        if check_if_valid(coor_input):
+            if check_word(word, used_word_list):
+                if word in word_list:
+                    score += cauculate_score(score, word)
+                    used_list.append(word)
+                    print(SEC_MSG)
+                else:
+                    print(LOS_MSG)
+            else:
+                print("You already found this word")
+        else:
+            print("Invalid move")
+    print("Finish msg + score")
+    play_again = input("Would you like to play again?")
+    if play_again == "Y":
+        main()
+    else:
+        print(QUIT_MSG)
+        quit()
 
-class Board(object):
-    def __init__(self):
-        self.board = boggle_board_randomizer
-        self.words = self.words_list
-        self.col_size = BOARD_SIZE
-        self.row_size = BOARD_SIZE
+def words_list(file):
+    with open(file) as boogle_dict:
+        words = []
+        for line in boogle_dict:
+            words.append(line.strip())
+        return words
 
-    def generate_board(self):
-        board = self.board
-        return board
+def convert_to_word(user_input,dict_board):
+    word = ''
+    for coor in user_input():
+        word = word + dict_board[coor]
+    return word
 
-    def words_list(self, filepath):
-        with open(filepath) as boogle_dict:
-            words = []
-            for line in boogle_dict:
-                words.append(line.strip())
-            return words
+def convert_to_dict(board):
+    coor_lst = [(i, j) for i in range(4) for j in range(4)]
+    coor_lst = [coor_lst[x:x + 4] for x in range(0, len(coor_lst), 4)]
+    dict_letter_coor = {}
+    for i in range(4):
+        for j in range(4):
+            dict_letter_coor[coor_lst[i][j]] = self.board[i][j]
+    return dict_letter_coor
 
-    def convert_to_coor(self):
-        coor_lst = [(i, j) for i in range(4) for j in range(4)]
-        coor_lst = [coor_lst[x:x + 4] for x in range(0, len(coor_lst), 4)]
-        dict_letter_coor = {}
-        for i in range(4):
-            for j in range(4):
-                dict_letter_coor[coor_lst[i][j]] = self.board[i][j]
-        return dict_letter_coor
+def check_if_valid(coor_lst):
 
-class Cube(object):
-    def __init__(self, size, face, shape):
-        self.size = size
-        self.face = face
-        self.shape = shape
+    if len(coor_lst) == len(set(coor_lst)):
+        return True
+    else:
+        return False
 
-
-class Player(object):
-    """
-
-    """
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-        self.word_list = []
-
-    def number_of_words(self):
-        return len(self.word_list)
-
-    def add_word(self, word):
-        self.word_list.append(word)
-        self.calculate_score(word)
-
-    def calculate_score(self, word):
-        return len(word) ** 2
-
-
-# make sure word is valid
-def check_word(word, player):
+def check_word(word, used_word_list):
     # make sure word is on the board
     # make sure word is in the dictionary
-    if word in player.word_list:
+    if word in used_word_list:
         return False
     return True
 
+def calculate_score(word):
+    amount = len(word) ** 2
+    return amount
 
-def main():
-    input = None
-    options = ['y', 'n']
-
-    input = raw_input("Would you like to play a game? (y/n): ").strip()
-    while (input not in options):
-        print("you typed %s. Please type 'y' or 'n'" % input)
-        input = None
-        input = raw_input("Would you like to play a game? (y/n): ").strip()
-
-    playing = input.lower().startswith('y')
-
-    if playing:
-        # game setup
-        player = Player("Chad")
-        board = Board()
-    while (playing):
-        board.display()
-        # would be nice to highlight the letter(s) typed to go along with the user
-        # input, and only allow letters that are on the board
-        # so if there are multiple 'E's, then they are all highlighted when 'e' is typed
-        # and if the next letter is 's', then only highlight the places on the board
-        # where E-S are neighbours.
-        # Then, if followed by another 'e', highlight only the E-S-E pattern (if it
-        # exists) else do not allow another 'e' to be entered.
-        word = raw_input("Enter a word you want to score: ").strip()
-        if check_word(word, player):
-            player.add_word(word, score_word(word))
-        else:
-            print("You've already scored that word. Try again.\n")
-
-        if player.num_words() == 5:
-            break
-    if playing:
-        player.print_score()
-
-    exit_message()
-
+def number_of_words(used_word_list):
+    return len(used_word_list)
 
 if __name__ == '__main__':
     main()
 
 
 
-
-
-def check_if_valid(self,coor_lst):
-
-    if len(coor_lst) == len(set(coor_lst)):
-        return True
-    else:
-        return False
-def cauculate_score(self):
