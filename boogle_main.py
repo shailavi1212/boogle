d75c93b6ba05af6460d304ac777bf33d68264252
eldadch <59150092+eldadch@users.noreply.github.com>
2020-01-27 00:45:06 +0200

    Add files via upload

--- boogle_main.py
+++ boogle_main.py
@@ -2,7 +2,8 @@ import sys
 import boggle_board_randomizer
 import time
 from tkinter import *
-import winsound
+
+# import winsound
 
 START_MSG = "Let's play Boogle!"
 SEC_MSG = "Great job! keep going :)"
@@ -11,51 +12,59 @@ QUIT_MSG = "Thanks for playing! hope to see you soon"
 BOARD_SIZE = 4
 FILEPATH = 'boggle_dict.txt'
 
+
 class Scroll_bar:
     def __init__(self):
-        table_frame = Frame( bd=30, bg='white', width=20, height=100)
+        table_frame = Frame(bd=30, bg='white', width=20, height=100)
         table_frame.place(x=800, y=150)
         self.table = Scrollbar(table_frame)
         self.table.pack(side=RIGHT, fill=Y)
-        self.list_of_words = Listbox(table_frame, yscrollcommand = self.table.set,bg = 'white',bd = 20,width = 10,height= 10,font = 'helvetica 15')
+        self.list_of_words = Listbox(table_frame, yscrollcommand=self.table.set, bg='white', bd=20, width=10, height=10,
+                                     font='helvetica 15')
         self.num_of_word = 0
         for i in range(100):
             self.list_of_words.insert(END, '' + str(i + 1))
         self.list_of_words.pack(side=LEFT)
         self.table.config(command=self.list_of_words.yview)
 
-
-    def add_word(self,word):
+    def add_word(self, word):
         self.list_of_words.delete(self.num_of_word)
-        self.list_of_words.insert(self.num_of_word,'' + str(self.num_of_word + 1) + ' ' + word)
+        self.list_of_words.insert(self.num_of_word, '' + str(self.num_of_word + 1) + ' ' + word)
         self.num_of_word = self.num_of_word + 1
 
+
 class Screen():
-    def __init__(self, master,screen_input):
+    def __init__(self, master, screen_input):
         self.bg = 'powder blue'
-        self.text_display = Label(master, font='ariel 20', textvariable=screen_input,
-                                    bg= 'white', justify='center')
+        self.text_display = Label(master, font='david 20', textvariable=screen_input,
+                                  bg='white', justify='center')
         self.text_display.grid()
+
     def right_answer(self):
-        #winsound.PlaySound('sweet-notification-alert_C#_major.wav',winsound.SND_ASYNC)
-        self.text_display.config(bg = 'green')
+        # winsound.PlaySound('sweet-notification-alert_C#_major.wav',winsound.SND_ASYNC)
+        self.text_display.config(bg='green')
 
     def not_valid(self):
-        #winsound.PlaySound('wrong-answer_F#_major.wav', winsound.SND_ASYNC)
+        # winsound.PlaySound('wrong-answer_F#_major.wav', winsound.SND_ASYNC)
         self.text_display.config(bg='red')
 
     def new_word(self):
         self.text_display.config(bg='white')
 
-class Score():
-    def __init__(self,master,score):
+
+class Score:
+    def __init__(self, master, score):
         self.score = score
-        score_frame = Label(master, textvariable = self.score, bg='white', font='ariel 25')
+        score_frame = Label(master, textvariable=self.score, bg='white', font='ariel 25')
         score_frame.pack()
+
     def get_score(self):
         return self.score
-    def set_score(self,new_score):
+
+    def set_score(self, new_score):
         self.score = new_score
+
+
 def main():
     """"""
     board = boggle_board_randomizer.randomize_board()
@@ -63,9 +72,9 @@ def main():
     word_list = words_list(FILEPATH)
     used_word_list = []
     score = 0
-    coor_input = input() # list of coor
+    coor_input = input()  # list of coor
     coor_input = convert_input_to_coor_lst(coor_input)
-    word = convert_to_word(coor_input,dict_board)
+    word = convert_to_word(coor_input, dict_board)
     start = time.time()
 
     while time.time() - start < 30:
@@ -91,6 +100,7 @@ def main():
         print(QUIT_MSG)
         quit()
 
+
 def words_list(file):
     """"""
     with open(file) as boogle_dict:
@@ -99,22 +109,25 @@ def words_list(file):
             words.append(line.strip())
         return words
 
+
 def convert_input_to_coor_lst(input):
     """"""
     lst = list()
-    for i in range(0,len(input),2):
-        lst.append((int(input[i]),int(input[i+1])))
+    for i in range(0, len(input), 2):
+        lst.append((int(input[i]), int(input[i + 1])))
     return lst
 
-def convert_to_word(user_input,dict_board):
+
+def convert_to_word(user_input, dict_board):
     """"""
     word = ''
     print(user_input)
     for coor in user_input:
         word = word + dict_board[coor]
-    #word = ''.join(str(i) for i in lst)
+    # word = ''.join(str(i) for i in lst)
 
     return word
+
 
 def convert_to_dict(board):
     """"""
@@ -126,22 +139,26 @@ def convert_to_dict(board):
             dict_letter_coor[coor_lst[i][j]] = board[i][j]
     return dict_letter_coor
 
+
 def check_if_valid(coor_lst):
     """"""
     if len(coor_lst) < 2:
         return True
-    if compare_last(coor_lst[-1],coor_lst[-2]) == 1:
+    if compare_last(coor_lst[-1], coor_lst[-2]) == 1:
         if len(coor_lst) == len(set(coor_lst)):
             return True
         else:
             return False
     else:
         return False
-def compare_last(last_coor,before_last):
+
+
+def compare_last(last_coor, before_last):
     if abs(last_coor[0] - before_last[0]) <= 1 and abs(last_coor[1] - before_last[1]) <= 1:
         return True
     else:
         return False
+
 
 def check_word(word, used_word_list):
     """"""
@@ -149,14 +166,17 @@ def check_word(word, used_word_list):
         return False
     return True
 
+
 def calculate_score(word):
     """"""
     amount = len(word) ** 2
     return amount
 
+
 def number_of_words(used_word_list):
     """"""
     return len(used_word_list)
+
 
 if __name__ == '__main__':
     main()
