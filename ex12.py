5c0b67d36a7c6d709ca00aa3a9c16be2d501d9de
shailavi1212 <59308932+shailavi1212@users.noreply.github.com>
2020-01-26 23:38:52 +0200

    Add files via upload

--- ex12.py
+++ ex12.py
@@ -10,124 +10,127 @@ root = Tk()
 root.geometry('1352x652+0+0')
 root.configure(background = 'white' )
 
-coor_list = list()
-board = boggle_board_randomizer.randomize_board()
-board_dict = boogle_main.convert_to_dict(board)
-screen_input = StringVar()
-word_list = boogle_main.words_list(boogle_main.FILEPATH)
-score_output = IntVar()
-score_output.set(0)
-used_word_list = []
-num_of_word = 0
+
 ##################################################### commands ###################################################################
 
 
+class Boogle:
+    def __init__(self,master):
+        self.background = Frame(master)
+        self.background.grid()
+        self.empty_frame = Frame(master,bg = 'white',width = 200,height= 600)
+        self.empty_frame.grid(row = 0, column = 0)
+        self.board_frame = Frame(master,bg = 'yellow',width = 440,height= 800 )
+        self.board_frame.grid(row = 0, column = 1)
 
-background = Frame(root)
-background.grid()
-empty_frame = Frame(root,bg = 'white',width = 200,height= 600)
-empty_frame.grid(row = 0, column = 0)
-board_frame = Frame(root,bg = 'yellow',width = 440,height= 800 )
-board_frame.grid(row = 0, column = 1)
-
-score_frame =Frame(root ,bd = 20, bg = 'white',width = 80,height= 80 )
-score_frame.place(x = 872, y = 70)
+        self.score_frame =Frame(master ,bd = 20, bg = 'white',width = 80,height= 80 )
+        self.score_frame.place(x = 872, y = 70)
 
 ############################################################### board frame ##############################################################################################
-timer_frame = Frame(board_frame,bd = 20,bg = 'white',width = 100,height= 100 )
-timer_frame.grid(row = 0, column = 0)
-screen_frame = Frame(board_frame)
-screen_frame.grid(row = 1, column = 0)
-letters_frame = Frame(board_frame,bd = 20,bg = 'white',width = 440,height= 400)
-letters_frame.grid(row = 2, column = 0)
-empty_space = Frame(board_frame,bd = 20,bg = 'white',width = 440,height= 30)
-empty_space.grid(row = 3, column = 0)
-submit_frame = Frame(board_frame,bd = 20,bg = 'white',width = 90,height= 50 )
-submit_frame.grid(row = 4, column = 0)
+        self.timer_frame = Frame(self.board_frame,bd = 20,bg = 'white',width = 100,height= 100 )
+        self.timer_frame.grid(row = 0, column = 0)
+        self.screen_frame = Frame(self.board_frame)
+        self.screen_frame.grid(row = 1, column = 0)
+        self.letters_frame = Frame(self.board_frame,bd = 20,bg = 'white',width = 440,height= 400)
+        self.letters_frame.grid(row = 2, column = 0)
+        self.empty_space = Frame(self.board_frame,bd = 20,bg = 'white',width = 440,height= 30)
+        self.empty_space.grid(row = 3, column = 0)
+        self.submit_frame = Frame(self.board_frame,bd = 20,bg = 'white',width = 90,height= 50 )
+        self.submit_frame.grid(row = 4, column = 0)
 ################################################ empty_side  ##########################################################################
-empty_space1 = Frame(empty_frame,bd = 20,width = 200,height= 500)
-empty_space1.grid(row = 0,column = 0)
-end_game_menu = Frame(empty_frame,bd = 20,width = 100,height= 100)
-end_game_menu.place(x = 0, y = 450)
+        self.empty_space1 = Frame(self.empty_frame,bd = 20,width = 200,height= 500)
+        self.empty_space1.grid(row = 0,column = 0)
+        self.end_game_menu = Frame(self.empty_frame,bd = 20,width = 100,height= 100)
+        self.end_game_menu.place(x = 0, y = 450)
+###################################################################### variavle################################################
+        self.coor_list = list()
+        self.board = boggle_board_randomizer.randomize_board()
+        self.board_dict = boogle_main.convert_to_dict(self.board)
+        self.screen_input = StringVar()
+        self.word_list = boogle_main.words_list(boogle_main.FILEPATH)
+        self.score_output = IntVar()
+        self.score_output.set(0)
+        self.used_word_list = []
+        self.num_of_word = 0
+
 ###################################################################### objects ###############################################
-scroll = boogle_main.Scroll_bar()
-screen = boogle_main.Screen(screen_frame,screen_input)
-score = boogle_main.Score(score_frame,score_output)
-
+        self.scroll = boogle_main.Scroll_bar()
+        self.screen = boogle_main.Screen(self.screen_frame,self.screen_input)
+        self.score = boogle_main.Score(self.score_frame,self.score_output)
+        self.exit_button = Button(self.end_game_menu, text='main menu', command=self.back_to_main)
+        self.exit_button.grid(row=1, column=0, sticky=N)
+        self.submit_button = Button(self.submit_frame, text='submit', font='david 14', command=self.end_of_word)
+        self.submit_button.place(x=-10, y=-15)
+        for col in range(4):
+            for row in range(4):
+                button = Button(self.letters_frame, text=self.board_dict[(col, row)], fg='red', width=2, height=2,
+                                command=lambda x=col, y=row: self.button_clicked(x, y))
+                button.grid(row=col, column=row)
+        ############################################################ display #######################################################
+        self.label = Label(self.timer_frame, text='30', bg='white', font='ariel 25')
+        self.label.place(x=10, y=15)
+        self.countdown(180)
 ###############################################################################################################################3
-def time_is_up():
+    def time_is_up(self):
 
-    answer = tkinter.messagebox.askquestion('','would you like to play again')
-    if answer == 'yes':
-        pass
-        #new game
-    else:
+        answer = tkinter.messagebox.askquestion('','would you like to play again')
+        if answer == 'yes':
+            pass
+            #new game
+        else:
+            pass
+            #back to main menu
+    def back_to_main(self):
         pass
         #back to main menu
-def back_to_main():
-    pass
-    #back to main menu
-def countdown(count):
-    label['text'] = count
+    def countdown(self,count):
+        self.label['text'] = count
 
-    if count > 0:
-        timer_frame.after(1000, countdown, count-1)
-    else:
-        time_is_up()
-def end_of_word():
-    word = boogle_main.convert_to_word(coor_list, board_dict)
-    if boogle_main.check_word(word, used_word_list):
-        if word in word_list:
-            screen.right_answer()
-            score_temp = score_output.get() + boogle_main.calculate_score(word)
-            score_output.set(score_temp)
-            used_word_list.append(word)
-            scroll.add_word(word)
-            coor_list.clear()
-            return
+        if count > 0:
+            self.timer_frame.after(1000, self.countdown, count-1)
         else:
-            not_valid()
-    else:
-        not_valid()
+            self.time_is_up()
+    def end_of_word(self):
+        word = boogle_main.convert_to_word(self.coor_list, self.board_dict)
+        if boogle_main.check_word(word, self.used_word_list):
+            if word in self.word_list:
+                self.screen.right_answer()
+                score_temp = self.score_output.get() + boogle_main.calculate_score(word)
+                self.score_output.set(score_temp)
+                self.used_word_list.append(word)
+                self.scroll.add_word(word)
+                self.coor_list.clear()
+                return
+            else:
+                self.not_valid()
+        else:
+            self.not_valid()
 
-    coor_list.clear()
-def not_valid():
-    screen.not_valid()
-    coor_list.clear()
+        self.coor_list.clear()
+    def not_valid(self):
+        self.screen.not_valid()
+        self.coor_list.clear()
 
-def button_clicked(x,y):
-    coor_list.append((x,y))
-    add_letter(coor_list)
-    if boogle_main.check_if_valid(coor_list) == False:#here we can do some sign
-        not_valid()
+    def button_clicked(self,x,y):
+        self.coor_list.append((x,y))
+        self.add_letter(self.coor_list)
+        if boogle_main.check_if_valid(self.coor_list) == False:#here we can do some sign
+            self.not_valid()
 
-
-    print(coor_list)
-def add_letter(coor_list):
-    if len(coor_list) == 1:
-        screen.new_word()
-    display = ""
-    for word in coor_list:
-        display = display + board_dict[word]
-    screen_input.set(display)
-################################################# words_table #################################################################
+    def add_letter(self,coor_list):
+        if len(coor_list) == 1:
+            self.screen.new_word()
+        display = ""
+        for word in coor_list:
+            display = display + self.board_dict[word]
+        self.screen_input.set(display)
+    ################################################# words_table #################################################################
 
 
-#################################################################### buttons ####################################################
-exit_button = Button(end_game_menu,text = 'main menu',command = back_to_main)
-exit_button.grid(row = 1, column = 0,sticky = N)
-submit_button = Button(submit_frame,text = 'submit',font = 'david 14',command = end_of_word)
-submit_button.place(x = -10,y = -15)
-for col in range (4):
-    for row in range(4):
-        button = Button(letters_frame, text = board_dict[(col,row)],fg = 'red',width = 2, height= 2,
-                         command = lambda x = col,y = row:button_clicked(x,y))
-        button.grid(row = col,column = row)
-############################################################ display #######################################################
-label = Label(timer_frame,text = '30',bg = 'white', font = 'ariel 25')
-label.place(x = 10, y = 15)
-countdown(180)
+    #################################################################### buttons ####################################################
+
 
 
 #################################################################### END ####################################################3
+boogle = Boogle(root)
 root.mainloop()
